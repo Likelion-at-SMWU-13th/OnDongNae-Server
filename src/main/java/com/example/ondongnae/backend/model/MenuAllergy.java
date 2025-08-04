e97eeb54e9276faf86f162c12e7834e091546103
ruddmslee <dlruddms326@naver.com>
2025-08-05 01:55:53 +0900

    Chore: Setter 제거

--- src/main/java/com/example/ondongnae/backend/model/MenuAllergy.java
+++ src/main/java/com/example/ondongnae/backend/model/MenuAllergy.java
@@ -4,11 +4,9 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class MenuAllergy {
