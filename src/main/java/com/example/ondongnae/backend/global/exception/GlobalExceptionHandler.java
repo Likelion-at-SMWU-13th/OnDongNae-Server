a91c1d1bee22b30da423fb54eb9115806ca972cd
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-08-09 14:35:46 +0900

    feat: ErrorCode, BaseException, 전역 예외 처리 추가

--- src/main/java/com/example/ondongnae/backend/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/ondongnae/backend/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.ondongnae.backend.global.exception;
 
+import com.example.ondongnae.backend.global.response.ApiResponse;
 import com.example.ondongnae.backend.global.response.CustomResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,17 +11,28 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<CustomResponse<?>> methodArgumentNotValidException(MethodArgumentNotValidException e) {
-        return ResponseEntity.badRequest()
-                .body(CustomResponse.badRequest("데이터가 누락되었습니다"));
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<CustomResponse<?>> illegalArgumentException(IllegalArgumentException e) {
+    // 커스텀 예외
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ApiResponse<?>> handleBaseException(BaseException e) {
+        ErrorCode errorCode = e.getErrorCode();
         return ResponseEntity
-                .status(HttpStatus.UNPROCESSABLE_ENTITY)
-                .body(CustomResponse.unprocessableEntity(e.getMessage()));
+                .status(errorCode.getStatus())
+                .body(ApiResponse.error(errorCode));
     }
 
+    // 유효성 검증 실패
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<?>> handleValidationException(MethodArgumentNotValidException e) {
+        return ResponseEntity
+                .status(ErrorCode.INVALID_INPUT_VALUE.getStatus())
+                .body(ApiResponse.error(ErrorCode.INVALID_INPUT_VALUE, e.getBindingResult().toString()));
+    }
+
+    // 예상 못한 예외
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<?>> handleException(Exception e) {
+        return ResponseEntity
+                .status(ErrorCode.INTERNAL_SERVER_ERROR.getStatus())
+                .body(ApiResponse.error(ErrorCode.INTERNAL_SERVER_ERROR, e.getMessage()));
+    }
 }
