53f05abc4477eed88fc1757e320cf66a260ae3ad
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-08-11 23:50:15 +0900

    feat: 수기 메뉴 등록용 dto 생성

---
+++ src/main/java/com/example/ondongnae/backend/menu/dto/ManualMenuCreateResponse.java
@@ -0,0 +1,14 @@
+package com.example.ondongnae.backend.menu.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class ManualMenuCreateResponse {
+    private List<Long> menuIds;
+}
