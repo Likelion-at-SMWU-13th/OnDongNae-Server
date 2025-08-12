2b2ec6657a7d72a608c9a1c1e730b3a828503490
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-08-12 22:12:14 +0900

    feat: BusinessHourController 생성

--- src/main/java/com/example/ondongnae/backend/store/controller/BusinessHourController.java
+++ src/main/java/com/example/ondongnae/backend/store/controller/BusinessHourController.java
@@ -2,14 +2,12 @@ package com.example.ondongnae.backend.store.controller;
 
 import com.example.ondongnae.backend.global.response.ApiResponse;
 import com.example.ondongnae.backend.store.dto.BusinessHourRequest;
+import com.example.ondongnae.backend.store.dto.BusinessHourResponse;
 import com.example.ondongnae.backend.store.service.BusinessHourService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -24,6 +22,12 @@ public class BusinessHourController {
     public ResponseEntity<ApiResponse<Map<String, Integer>>> save(@Valid @RequestBody BusinessHourRequest request) {
         int saved = businessHourService.saveBusinessHour(request);
         return ResponseEntity.ok(ApiResponse.ok("영업시간 저장 완료", Map.of("saved", saved)));
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<BusinessHourResponse>> get() {
+        BusinessHourResponse businessHourResponse = businessHourService.getBusinessHour();
+        return ResponseEntity.ok(ApiResponse.ok("영업시간 조회 성공", businessHourResponse));
     }
 
 }
