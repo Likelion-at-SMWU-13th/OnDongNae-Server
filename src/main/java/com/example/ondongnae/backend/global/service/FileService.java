bcaaa423b8ee2a016ef0c94506b18f2a72b8abd1
ruddmslee <dlruddms326@naver.com>
2025-08-13 03:57:41 +0900

    Feat: 이미지 URL 수정

--- src/main/java/com/example/ondongnae/backend/global/service/FileService.java
+++ src/main/java/com/example/ondongnae/backend/global/service/FileService.java
@@ -18,17 +18,16 @@ public class FileService {
     }
 
     @Value("${AWS_BUCKET}")
-    private String bucket;
+    public String bucket;
 
     public String uploadFile(MultipartFile file) {
         try {
             String fileName = file.getOriginalFilename();
-            String fileUrl = "https://" + bucket + "/" + fileName;
             ObjectMetadata metadata = new ObjectMetadata();
             metadata.setContentType(file.getContentType());
             metadata.setContentLength(file.getSize());
             s3Client.putObject(bucket, fileName, file.getInputStream(), metadata);
-            return fileUrl;
+            return s3Client.getUrl(bucket, fileName).toString();
         } catch (IOException e) {
             e.printStackTrace();
         }
