fb69499b008b126a5bf02ab7e2188fa9aa3c2b15
ruddmslee <dlruddms326@naver.com>
2025-08-18 20:57:36 +0900

    Feat: refresh token으로 토큰 재발급

--- src/main/java/com/example/ondongnae/backend/member/controller/AuthController.java
+++ src/main/java/com/example/ondongnae/backend/member/controller/AuthController.java
@@ -75,4 +75,10 @@ public class AuthController {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(ApiResponse.ok("대분류에 해당하는 모든 소분류를 불러왔습니다", subCategories));
     }
+
+    @PostMapping("/token/refresh")
+    public ResponseEntity<ApiResponse<?>> reissue(@RequestBody String refreshToken) {
+        TokenDto newTokens = authService.reissue(refreshToken);
+        return ResponseEntity.ok().body(ApiResponse.ok(newTokens));
+    }
 }
