1266560cd3162e991f54933b657bf95da685d0ed
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-08-18 22:54:12 +0900

    feat: 암호, 토큰 관련 코드 복붙

--- src/main/java/com/example/ondongnae/backend/member/controller/AuthController.java
+++ src/main/java/com/example/ondongnae/backend/member/controller/AuthController.java
@@ -77,10 +77,10 @@ public class AuthController {
                 .body(ApiResponse.ok("대분류에 해당하는 모든 소분류를 불러왔습니다", subCategories));
     }
 
-    // 내 정보 조회
-    @GetMapping("/profile")
-    public ResponseEntity<ApiResponse<MyProfileResponse>> getMyProfile() {
-        var data = storeService.getMyProfile();
-        return ResponseEntity.ok(ApiResponse.ok("내 정보 조회 성공", data));
+    @PostMapping("/token/refresh")
+    public ResponseEntity<ApiResponse<?>> reissue(@RequestBody String refreshToken) {
+        TokenDto newTokens = authService.reissue(refreshToken);
+        return ResponseEntity.ok().body(ApiResponse.ok(newTokens));
     }
+
 }
---
+++ src/main/java/com/example/ondongnae/backend/member/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package com.example.ondongnae.backend.member.repository;
+
+import com.example.ondongnae.backend.member.model.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByMemberId(Long memberId);
+}
