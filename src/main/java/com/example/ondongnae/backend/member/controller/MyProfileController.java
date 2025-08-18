e202e064cbd5867ff273c2d7c3ec1990649e3885
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-08-18 22:57:08 +0900

    feat: MyProfileController 생성

---
+++ src/main/java/com/example/ondongnae/backend/member/controller/MyProfileController.java
@@ -0,0 +1,34 @@
+package com.example.ondongnae.backend.member.controller;
+
+import com.example.ondongnae.backend.global.response.ApiResponse;
+import com.example.ondongnae.backend.member.dto.MyProfileResponse;
+import com.example.ondongnae.backend.member.dto.MyProfileUpdateRequest;
+import com.example.ondongnae.backend.store.service.StoreService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/me/profile")
+public class MyProfileController {
+
+    private final StoreService storeService;
+
+    // 내 정보 조회
+    @GetMapping
+    public ResponseEntity<ApiResponse<MyProfileResponse>> getMyProfile() {
+        var data = storeService.getMyProfile();
+        return ResponseEntity.ok(ApiResponse.ok("내 정보 조회 성공", data));
+    }
+
+    // 내 정보 수정
+    @PatchMapping
+    public ResponseEntity<ApiResponse<Void>> updateMyProfile(
+            @Valid @RequestBody MyProfileUpdateRequest req) {
+        storeService.updateMyProfile(req);
+        return ResponseEntity.ok(ApiResponse.ok("수정 완료", null));
+    }
+
+}
